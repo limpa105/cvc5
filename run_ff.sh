02ffc36143370661334c7aaf68adb759cc312f83
Liza Pertseva <pertseva@barrett2.stanford.edu>
2024-05-05 19:59:46 -0700

    fixed analysis script

--- run_ff.sh
+++ run_ff.sh
@@ -6,18 +6,27 @@ output_file=$2
 solved=0
 total=0
 
+
+if [ ! -f "$output_file" ]; then
+    # The file doesn't exist, so create it using touch
+    touch "$output_file"
+else
+    > "$output_file"
+fi
+    
 for file in "$TEST_DIR"/*; do
     if [ -f "$file" ]; then
         total=$((total+1))
-        echo -n >> "$output_file"
+        echo  >> "$output_file"
 	    timeout 120 $CVC5 --ff-range-solver "$file" 
         if [ $? -eq 0 ]; then
+		echo -n "$file solved" >> $output_file
     # If the program did not timeout, increment the counter
             solved=$((solved+1))
         else
-            echo 
+	    echo "$file failed" >> $output_file
         # If the program timed out, do nothing
         fi
     fi
 done 
-echo -n "Solved: $solved out of $total"
+echo  "Solved: $solved out of $total"
